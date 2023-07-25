b8a3ac0cb51d3c049a83c8afacaa4f047200bbf2
PolinaLegusheva <polina.legusheva.003@mail.ru>
2023-07-25 18:41:25 +0300

    saving an image

--- project/main.py
+++ project/main.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, request, session, Flask, redirect
+from flask import Blueprint, render_template, request, session
 from flask_login import login_required, current_user, LoginManager
+from run import app
 from db import db
 import os
-
-from flask_sqlalchemy import SQLAlchemy
+from models import User
+from auth import auth as auth_blueprint
+from flask import send_file
 from werkzeug.utils import secure_filename
 
 # WSGI Application
@@ -12,6 +14,7 @@ UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
 
 main = Blueprint('main', __name__, template_folder='templates', static_folder='staticFiles')
 
+last_uploaded_file = None
 
 @main.route('/profile')
 @login_required
@@ -27,6 +30,7 @@ def index():
 @main.route('/', methods=("POST", "GET"))
 def uploadFile():
     if request.method == 'POST':
+        global last_uploaded_file
         # Upload file flask
         uploaded_img = request.files['uploaded-file']
         # Extracting uploaded data file name
@@ -35,6 +39,7 @@ def uploadFile():
         uploaded_img.save(os.path.join(UPLOAD_FOLDER, img_filename))
         # Storing uploaded file path in flask session
         session['uploaded_img_file_path'] = os.path.join(UPLOAD_FOLDER, img_filename)
+        last_uploaded_file = img_filename
 
         return render_template('upload_display_img2.html')
 
@@ -47,10 +52,14 @@ def displayImage():
     return render_template('show_img.html', user_image=img_file_path)
 
 
-app = Flask(__name__)
+@app.route('/download')
+def download():
+    global last_uploaded_file
+    if last_uploaded_file:
+        return send_file(os.path.join(UPLOAD_FOLDER, last_uploaded_file), as_attachment=True)
+    else:
+        return '<h1>No files uploaded yet</h1>'
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 db.init_app(app)
 
@@ -58,21 +67,15 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
-from models import User
-
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
-
-
 app.register_blueprint(main)
 
-import models
 
 with app.app_context():
     db.create_all()
